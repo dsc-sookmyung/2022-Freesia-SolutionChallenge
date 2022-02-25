b9f305c4adc60ae0c0b135059c952390af8ef65f
nrj022 <0602smj@gmail.com>
2022-02-25 14:43:58 +0900

    [front] Fix ranking components in challenge.tsx

--- frontend/src/components/challenge/Challenge.tsx
+++ frontend/src/components/challenge/Challenge.tsx
@@ -9,15 +9,14 @@ import {
   Dimensions,
   TouchableOpacity,
 } from "react-native";
-import { Divider, ProfileIcon } from "../../CommonComponent";
+import { Divider } from "../../CommonComponent";
 import { Ionicons } from "@expo/vector-icons";
-import { theme } from "../../color";
 
 const numColumns = 3;
 const screenWidth = Dimensions.get("window").width;
 const screenPadding = 20;
 
-const DATA = [
+const recentPostData = [
   {
     id: "1",
     title: "first",
@@ -48,6 +47,29 @@ const DATA = [
   },
 ];
 
+const rankingData = [
+  {
+    rank: 0,
+    numberOfStickers: 5,
+  },
+  {
+    rank: 1,
+    numberOfStickers: 5,
+  },
+  {
+    rank: 2,
+    numberOfStickers: 5,
+  },
+  {
+    rank: 3,
+    numberOfStickers: 5,
+  },
+  {
+    rank: 4,
+    numberOfStickers: 5,
+  },
+];
+
 const rank: string[] = [
   "1st",
   "2nd",
@@ -68,6 +90,8 @@ export default function Map() {
     return (
       <View
         style={{
+          justifyContent: "center",
+          alignItems: "center",
           width: isUser ? 60 : 50,
           height: isUser ? 60 : 50,
           borderRadius: isUser ? 30 : 25,
@@ -75,6 +99,8 @@ export default function Map() {
           backgroundColor: "grey",
           overflow: "hidden",
           elevation: 2,
+          borderWidth: isUser ? 3 : 0,
+          borderColor: "#BAF1C9",
         }}
       >
         <Image
@@ -93,12 +119,19 @@ export default function Map() {
     );
   };
 
-  const Ranking = ({ rank }) => (
+  const Ranking = ({ rank, numberOfStickers, isUser, imagePath }) => (
     <TouchableOpacity activeOpacity={0.8} style={styles.ranking}>
-      <ProfileIcon
-        imagePath={require("../../../assets/tori.jpg")}
-        isUser={false}
-      />
+      <ProfileIcon imagePath={imagePath} isUser={isUser} />
+
+      <View style={styles.numberOfStickers}>
+        <Text>{numberOfStickers}</Text>
+      </View>
+      {rank == "1st" ? (
+        <Image
+          style={styles.crownImg}
+          source={require("../../../assets/crown.png")}
+        ></Image>
+      ) : null}
       <Text>{rank}</Text>
     </TouchableOpacity>
   );
@@ -112,17 +145,25 @@ export default function Map() {
         horizontal
         style={styles.rankingScrollView}
       >
-        <View style={styles.ranking}>
-          <ProfileIcon imagePath={null} isUser={true} />
-          <Text>me</Text>
-        </View>
-        {rank.map((r, idx) => (
-          <Ranking rank={r} key={idx} />
+        <Ranking
+          rank={"73st"}
+          imagePath={null}
+          isUser={true}
+          numberOfStickers={25}
+        />
+        {rankingData.map((r, idx) => (
+          <Ranking
+            rank={rank[r.rank]}
+            key={idx}
+            isUser={false}
+            imagePath={require("../../../assets/tori.jpg")}
+            numberOfStickers={r.numberOfStickers}
+          />
         ))}
       </ScrollView>
       <Divider />
       <FlatList
-        data={DATA}
+        data={recentPostData}
         renderItem={PostItem}
         keyExtractor={(item) => item.id}
         numColumns={numColumns}
@@ -152,6 +193,7 @@ const styles = StyleSheet.create({
   },
   rankingScrollView: { flexGrow: 0 },
   ranking: {
+    position: "relative",
     justifyContent: "flex-end",
     alignItems: "center",
   },
@@ -168,5 +210,23 @@ const styles = StyleSheet.create({
   },
   writePostIcon: {
     color: "#ffd25E",
+  },
+  numberOfStickers: {
+    position: "absolute",
+    right: 5,
+    bottom: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    elevation: 2,
+  },
+  crownImg: {
+    position: "absolute",
+    top: -10,
+    width: 50,
+    height: 50,
   },
 });
