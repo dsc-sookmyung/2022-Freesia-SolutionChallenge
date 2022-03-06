b1c78ab3af6b95087b8f7660f4fa42dcccc53ee4
cutehumanS2 <60508828+cutehumanS2@users.noreply.github.com>
2022-03-06 23:33:37 +0900

    [back] Fix Conflict

--- backend/imyourfreesia/src/main/java/com/freesia/imyourfreesia/domain/community/CommunityRepository.java
+++ backend/imyourfreesia/src/main/java/com/freesia/imyourfreesia/domain/community/CommunityRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface CommunityRepository extends JpaRepository<Community, Long> {
     List<Community> findByCategory(String category);
     List<Community> findByUidAndCategory(User user, String category);
+
 }
