d1d53bce97d27d4fe984b928c0a2bd65baa80cfd
cutehumanS2 <60508828+cutehumanS2@users.noreply.github.com>
2022-03-08 22:15:18 +0900

    [back] Add Challenge Entity

---
+++ backend/imyourfreesia/src/main/java/com/freesia/imyourfreesia/domain/challenge/Challenge.java
@@ -0,0 +1,59 @@
+package com.freesia.imyourfreesia.domain.challenge;
+
+import com.freesia.imyourfreesia.domain.BaseTimeEntity;
+import com.freesia.imyourfreesia.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Optional;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Challenge extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn (name = "uid")
+    private User uid;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String contents;
+
+    //@Column(nullable = false)
+    private String image;
+
+    @Builder
+    public Challenge(User uid, String title, String contents, String image){
+        this.uid = uid;
+        this.title = title;
+        this.contents = contents;
+        this.image = image;
+    }
+
+    public void setUid(User uid){
+        this.uid = uid;
+    }
+
+    public void setImage(String image){
+        this.image = image;
+    }
+
+    //public void update(String title, String contents, String image) {
+    public Challenge update(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+        //this.image = image;
+        return this;
+    }
+}
