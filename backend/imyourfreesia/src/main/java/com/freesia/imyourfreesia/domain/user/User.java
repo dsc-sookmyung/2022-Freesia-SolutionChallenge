9f028f557112fc3d1df4135c7a2ab27eecaab1d8
GaGa-Kim <wn8925@sookmyung.ac.kr>
2022-03-19 13:07:10 +0900

    [back] User Builder

--- backend/imyourfreesia/src/main/java/com/freesia/imyourfreesia/domain/user/User.java
+++ backend/imyourfreesia/src/main/java/com/freesia/imyourfreesia/domain/user/User.java
@@ -2,7 +2,6 @@ package com.freesia.imyourfreesia.domain.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.freesia.imyourfreesia.domain.BaseTimeEntity;
-import com.freesia.imyourfreesia.domain.challenge.Challenge;
 import lombok.*;
 
 import javax.persistence.*;
@@ -36,11 +35,6 @@ public class User extends BaseTimeEntity {
 
     private String profileImg;
 
-    /*@OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "goalMsgId")
-    //private String goalMsg;
-    private GoalMsg goalMsg;*/
-
     private boolean activated;
 
     @ManyToMany(cascade=CascadeType.ALL)
@@ -51,23 +45,20 @@ public class User extends BaseTimeEntity {
     private Set<Authority> authorities;
 
     @Builder
-    public User(String username, String loginId, String password, String email, String nickName, String profileImg){
+    public User(String username, String loginId, String password, String email, String nickName, String profileImg, boolean activated, Set<Authority> authorities){
         this.username = username;
         this.loginId = loginId;
         this.password = password;
         this.email = email;
         this.nickName = nickName;
         this.profileImg = profileImg;
-        // this.goalMsg = goalMsg;
+        this.activated = activated;
+        this.authorities = authorities;
     }
 
-    //public void update(String username, String password, String nickName, String profileImg, String goalMsg) {
     public User update(String nickName, String profileImg) {
-        //this.username = username;
-        //this.password = password;
         this.nickName = nickName;
         this.profileImg = profileImg;
-        //this.goalMsg = goalMsg;
         return this;
     }
 }
--- backend/imyourfreesia/src/main/java/com/freesia/imyourfreesia/domain/user/UserRepository.java
+++ backend/imyourfreesia/src/main/java/com/freesia/imyourfreesia/domain/user/UserRepository.java
@@ -1,7 +1,6 @@
 package com.freesia.imyourfreesia.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.List;
@@ -10,8 +9,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     User findByLoginIdAndPassword(String loginId, String password);
     Optional<User> findOneWithAuthoritiesByEmail(String email);
-    // List<User> findAllByEmail();
 
-    //@Query("SELECT email FROM User")
     List<User> findAll();
 }
