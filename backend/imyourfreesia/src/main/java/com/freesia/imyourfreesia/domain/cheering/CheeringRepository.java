67f6f65fe3afce9017df35f9c4315c6851badcb4
cutehumanS2 <60508828+cutehumanS2@users.noreply.github.com>
2022-03-20 19:41:54 +0900

    [back] Add Cheering API

--- backend/imyourfreesia/src/main/java/com/freesia/imyourfreesia/domain/cheering/CheeringRepository.java
+++ backend/imyourfreesia/src/main/java/com/freesia/imyourfreesia/domain/cheering/CheeringRepository.java
@@ -2,10 +2,13 @@ package com.freesia.imyourfreesia.domain.cheering;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;;
+import java.time.LocalDate;
+import java.util.List;;
 
 public interface CheeringRepository extends JpaRepository<Cheering, Long> {
     Long countByYourEmail(String userEmail);
+    List<Cheering> findByMyEmail(String userEmail);
+    Cheering findByMyEmailAndYourEmail(String myEmail, String yourEmail);
     //List<Cheering> findAllByCreatedDateBetweenAndYourEmail(LocalDateTime start, LocalDateTime end, String userEmail);
     Long countByCreatedDateBetweenAndYourEmail(LocalDate start, LocalDate end, String userEmail);
 }
